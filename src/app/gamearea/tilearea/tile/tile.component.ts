a013f0f218013d8858d96fcfa3a1b6a6cb8a14a5
meambobbo <meambobbo@foobazaar.com>
2020-05-14 23:13:58 -0500

    initial work on tilearea, wip

--- src/app/gamearea/tilearea/tile/tile.component.ts
+++ src/app/gamearea/tilearea/tile/tile.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tile.component.scss', '../../../app.component.scss']
 })
 export class TileComponent implements OnInit {
+  exposed: string;
+  icon: string;
+  matched: boolean;
 
   constructor() { }
 
+  onClick(): void {
+    //todo - send event and self to game service
+  }
+
   ngOnInit(): void {
+    this.exposed = 'todo';
+    this.icon = 'todo';
+    this.matched = false;
   }
 
 }
--- src/app/gamearea/tilearea/tilearea.component.ts
+++ src/app/gamearea/tilearea/tilearea.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TileComponent } from './tile/tile.component';
+
+const _tileCounts = 16;
 
 @Component({
   selector: 'app-tilearea',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TileareaComponent implements OnInit {
 
+  tiles: TileComponent[];
+
   constructor() { }
 
   ngOnInit(): void {
-  }
+  	var i: number;
 
+    // todo iterate over icons to initialize  
+  	for (i = 0; i < _tileCounts; i++) {
+      let tile: TileComponent = new TileComponent();
+  	  this.tiles.push(tile);
+  	}
+  }
 }
